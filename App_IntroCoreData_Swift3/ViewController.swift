5456ee60f49b4c02af33bbfccc00f0b15a6725f7
Felipe <info@icologic.co>
2017-04-04 17:03:15 +0200

    Mejoras

--- App_IntroCoreData_Swift3/ViewController.swift
+++ App_IntroCoreData_Swift3/ViewController.swift
@@ -7,11 +7,57 @@
 //
 
 import UIKit
+import CoreData
 
 class ViewController: UIViewController {
+    
+    
+    //MARK: - Variables locales
+    var contextManager : NSManagedObjectContext!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        
+        //1 -> Creamos la instancia a traves de la cual salvaremos datos
+        let customPersona = Persona(context: contextManager)
+        customPersona.nombre = "Andres"
+        customPersona.apellido = "Ocampo"
+        customPersona.email = "info@info.com"
+        customPersona.movil = 653940573
+        
+        
+        //2 -> Salvamos datos a traves del contexto
+        do{
+            try contextManager.save()
+            
+        }catch{
+            print("Error salvando datos")
+        }
+        
+        
+        //3 -> Recuperamos datos a traves del contexto
+        let request : NSFetchRequest<Persona> = Persona.fetchRequest()
+        let customPredicate = NSPredicate(format: "nombre = 'Andres'")
+        let cusotmSortDescriptor = NSSortDescriptor(key: "nombre", ascending: true)
+        
+        request.predicate = customPredicate
+        request.sortDescriptors = [cusotmSortDescriptor]
+        
+        do{
+            //4
+            let multitud = try contextManager.fetch(request)
+            for c_persona in multitud{
+                print("Nombre: \(c_persona.nombre!)\nApellido: \(c_persona.apellido!)\nEmail: \(c_persona.email!)\nMovil: \(c_persona.movil)\n\n")
+            }
+            
+        }catch{
+            print("Error cargando datos")
+        }
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
